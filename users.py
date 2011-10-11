1f89252b67826f90780575a19440d72bf6b4d5ff
Tom Thorogood <tom.thorogood@clearpointccs.org>
2011-10-10 22:49:13 -0700

    Started scripting user session data

--- users.py
+++ users.py
@@ -2,14 +2,17 @@ class User(object):
     pass
 
 def loginUser(username, password, db):
-    result = db.select(
-            'clearpointCalc_users',
-            where = "username='"+username+"' and password='"+password+"'",
-            what = "username,firstName"
-            )
+    query = "SELECT id,firstName FROM budgetCalculator_Users "
+    query += "WHERE email='"+username+"'  and password=PASSWORD('"+password+"')"
+    result = db.query(query)
     if result:
+        for row in result:
+            uID = row.id
+            name= row.firstName
         from web import setcookie
-        setcookie('username', username, 3600)
+        setcookie('username', username)
+        setcookie('id', uID)
+        setcookie('name', name)
         return True
     else:
         return False
