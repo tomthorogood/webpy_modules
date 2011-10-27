4fab4929927d84124cbd9d7ba6f88f7f16359837
Tom Thorogood <tom.thorogood@clearpointccs.org>
2011-10-27 12:30:59 -0400

    Can now add and edit accounts! Oh boy!

--- debt.py
+++ debt.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
 def retrieve_user_debt (uID, db):
-   query = "SELECT account_name, balance_due, interest_rate, minimum_payment, extra_payment FROM budgetCalculator_Debt "
-   query += "WHERE uID=%s" % (uID)
-   result = db.query(query)
+    query = "SELECT account_id, account_name, balance_due, interest_rate, minimum_payment, extra_payment FROM budgetCalculator_Debt "
+    query += "WHERE uID=%s" % (uID)
+    result = db.query(query)
 
-   user_accounts = {}
-   
-   if result:
-	for row in result:    
-	    user_accounts[row.account_name] = {'balance_due' : row.balance_due, 'interest_rate' : row.interest_rate, 'minimum_payment' : row.minimum_payment, 'extra_payment' : row.extra_payment }
-   return user_accounts	
+    user_accounts = {}
+
+    if result:
+        for row in result:    
+            user_accounts[row.account_name] = {'account_id': row.account_id, 'balance_due' : row.balance_due, 'interest_rate' : row.interest_rate, 'minimum_payment' : row.minimum_payment, 'extra_payment' : row.extra_payment }
+    return user_accounts	
 	    
 
