94487b2774d5f45b5ef329fa71ad5ac78a5fe5c8
Tom Thorogood <tom.thorogood@clearpointccs.org>
2011-11-29 14:47:20 -0500

    About to tear this baby down.

---
+++ income.py
@@ -0,0 +1,14 @@
+def retrieve_user_income (user_id, db):
+    user_income = {}
+
+    query = "SELECT source_id, source_name, source_amount, source_savings FROM budgetCalculator_Income WHERE uID=%s" % (user_id)
+    result = db.query(query)
+
+    if result:
+        for row in result:
+            user_income[row.source_name] = {
+                    'source_id' : row.source_id,
+                    'source_amount' : row.source_amount,
+                    'source_savings' : row.source_savings
+                    }
+    return user_income
