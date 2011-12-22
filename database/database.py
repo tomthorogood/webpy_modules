899af8bce00bff3366acc8ea528ea7defbfd6344
Tom Thorogood <tom@tomthorogood.com>
2011-12-22 11:03:46 -0500

    Added exists() method for User class to test whether a user already exists within a database

--- database/database.py
+++ database/database.py
@@ -188,6 +188,17 @@ class User(object):
     def link_income(self):
         self.income = Income_Source(self.get_id())
 
+    def exists(self, val):
+        """
+        Checks to see whether a username already exists in the database.
+        """
+        q = ["SELECT username FROM %s " % self.db.table, "WHERE username = PASSWORD('%s')" % Paramify(hash_this(val))]
+        r = self.db.query(q)
+        if not r or r[0].username != val:
+            return False
+        else:
+            return True
+
 class Money(object):
     def __init__(self, user_id, table):
         self.__user_id = user_id
